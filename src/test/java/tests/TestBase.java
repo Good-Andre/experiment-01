7c9060003c528ef4c58b411b91b371ca7e958acc
good_andre <good-andre@yandex.ru>
2024-10-26 17:00:08 +0300

    fix bug

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -41,9 +41,10 @@ public class TestBase {
 
     @AfterEach
     void addAttachments() {
-        Attach.screenshotAs("Last screenshot");
-        Attach.pageSource();
-        Attach.browserConsoleLogs();
+//        Attach.screenshotAs("Last screenshot");
+//        Attach.pageSource();
+//        Attach.browserConsoleLogs();
+
         // Attach.video();
     }
 
